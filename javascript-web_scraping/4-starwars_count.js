4e844157ca7249433c293fd8969c0cb5570e25d4
JDaniel26-Bory <juanrestrepo246@gmail.com>
2023-09-21 15:50:18 -0500

    add the files

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -5,8 +5,8 @@ const url = process.argv[2];
 
 request(url, (error, response, body) => {
   if (!error) {
-    const data = JSON.parse(body);
-    const count = data.results.filter((movie) =>
+    const data = JSON.parse(body).results;
+    const count = data.filter((movie) =>
       movie.characters.includes('https://swapi-api.hbtn.io/api/people/18/')
     );
     console.log(count.length);
